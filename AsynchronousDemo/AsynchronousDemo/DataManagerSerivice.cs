3098881351e1ef5dab91aef4ea1ac3eb9b7aba57
daiwenzhang <wenzhang.dai@gmail.com>
2020-11-21 14:38:45 +0800

    C#异步调用Demo

---
+++ AsynchronousDemo/AsynchronousDemo/DataManagerSerivice.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AsynchronousDemo
+{
+    public class DataManagerSerivice
+    {
+        /// <summary>
+        /// 异步回调结果
+        /// </summary>
+        IAsyncResult asyncResult = null;
+
+        private static DataManagerSerivice instance = new DataManagerSerivice();
+
+        private int data = -1;
+
+        private DataManagerSerivice()
+        {
+
+        }
+
+        public static DataManagerSerivice Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        public int Data
+        {
+            get
+            {
+                return this.data;
+            }
+
+            set
+            {
+                this.data = value;
+            }
+        }
+
+        public void DataWarmUp()
+        {
+            Action func = new Action(this.QueryData);
+
+            this.asyncResult = func.BeginInvoke(null, null);
+        }
+
+        public int GetData()
+        {
+            if (null == this.asyncResult)
+            {
+                return -1;
+            }
+
+            this.asyncResult.AsyncWaitHandle.WaitOne();
+            return this.Data;
+        }
+
+        private void QueryData()
+        {
+            Thread.Sleep(5000);
+            Random random = new Random();
+            this.Data = random.Next();
+        }
+    }
+}
---
+++ AsynchronousDemo/AsynchronousDemo/Form1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AsynchronousDemo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DataManagerSerivice.Instance.DataWarmUp();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Action action = new Action(this.QueryData);
+
+            action.BeginInvoke(null, null);            
+        }
+
+        private void QueryData()
+        {
+            int ret = DataManagerSerivice.Instance.GetData();
+            this.BeginInvoke(new Action(() => this.label1.Text = ret.ToString()), null);
+        }
+    }
+}
